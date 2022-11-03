232515549474d3eed4cacf6aca0b85a37865655d
Mathis Boulais <77585805+Asukyy@users.noreply.github.com>
2022-11-03 17:00:31 +0100

    add

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 function addTeams(){
     var search = document.getElementById("teams").value;
-    var div = document.getElementById("box").innerHTML += "<div class='btn2'>"+ search + "</div>" ;
-    document.getElementById("teams").value = "";
-    document.body.appendChild(div);
+    var div = document.getElementById("box").style.display = "block";
+    document.getElementById("teams").value = "";    var div = document.getElementById("box").innerHTML += "<div class='value'>" + "<div class='withoutx' >" + "<p class='textinput'>" + search + "<span class='delete' onclick=deleteForm()>" + "X" + "<span>"+"</div>" + "</p>" + "</div>";
 
+    document.body.appendChild(div);
+}
+
+function deleteForm() {
+    var div = document.getElementById("box");
+    div.removeChild(div.lastChild);
+    if (div.childElementCount == 0) {
+        document.getElementById("box").style.display = "none";
+    }
 }
